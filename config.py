5245e4413af62120adf91cbe3e52b690e13840f0
salverukeerthana <keerthanasalveru6.com>
2025-11-17 23:30:36 +0530

    config added

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 import os
 
 # EXACT same key that worked in the test above
-GEMINI_API_KEY = "AIzaSyDNRKOHPoWtDQgVN8K7wVSc-YULieBgPHY"
+GEMINI_API_KEY = "Enter your API key here"
 
 def get_api_key():
     """
